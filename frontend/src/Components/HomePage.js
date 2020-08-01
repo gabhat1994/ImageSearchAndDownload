36028dbd3eaa26510d0a2f437e0cccd0743cf670
stamic1 <bhatgaurav96@gmail.com>
2020-08-01 20:33:35 +0530

    pagination and display 9 items per page

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -1,36 +1,61 @@
 import SearchBar from "./SearchBar";
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import axios from "axios";
 import ImageList from "./ImageList";
 
-class HomePage extends Component {
-  state = {
-    images: [],
-  };
-  onSearchSubmit = async (term) => {
-    console.log(term, "term");
+function HomePage() {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const per_page = 9;
+
+  const onSearchSubmit = async (term) => {
     const response = await axios.get(
       "https://api.unsplash.com/search/photos/",
       {
-        params: { query: term },
+        params: { page: currentPage, per_page: per_page, query: term },
         headers: {
           Authorization:
             "Client-ID o7dYoGTAAYKjip1p_dFQ6BJvbbAvLuxY6v_SDnbxsCw",
         },
       }
     );
-
-    this.setState({ images: response.data.results });
+    setQuery(term);
+    setImages(response.data.results);
   };
-  render() {
-    return (
-      <div>
-        <SearchBar userSubmit={this.onSearchSubmit} />
-        <span>{this.state.images.length} images</span>
-        <ImageList foundImages={this.state.images} />
-      </div>
+
+  const onPageChange = async (term) => {
+    let page = currentPage + 1;
+    const response = await axios.get(
+      "https://api.unsplash.com/search/photos/",
+      {
+        params: { page: page, per_page: per_page, query: query },
+        headers: {
+          Authorization:
+            "Client-ID o7dYoGTAAYKjip1p_dFQ6BJvbbAvLuxY6v_SDnbxsCw",
+        },
+      }
     );
-  }
+    setCurrentPage(page);
+    setImages(response.data.results);
+  };
+
+  return (
+    <div>
+      <SearchBar userSubmit={onSearchSubmit} />
+      <br />
+      <ImageList foundImages={images} />
+      <br />
+      {images.length > 0 ? (
+        <button
+          style={{ marginTop: "564px", marginLeft: "644px" }}
+          onClick={onPageChange}
+        >
+          Load More
+        </button>
+      ) : null}
+    </div>
+  );
 }
 
 export default HomePage;
