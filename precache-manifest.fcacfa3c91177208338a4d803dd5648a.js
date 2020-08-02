0148ad5b71a720880dc6532ceed7fca4dde311f4
stamic1 <bhatgaurav96@gmail.com>
2020-08-02 17:25:34 +0530

    Updates

---
+++ precache-manifest.fcacfa3c91177208338a4d803dd5648a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a618484b01c46e533816bcc944f1cb89",
+    "url": "/ImageSearchAndDownload/index.html"
+  },
+  {
+    "revision": "1f0653f1502cac7c1f42",
+    "url": "/ImageSearchAndDownload/static/css/main.d9347e0a.chunk.css"
+  },
+  {
+    "revision": "0d55a74f14f9736c7e92",
+    "url": "/ImageSearchAndDownload/static/js/2.c8c218b5.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/ImageSearchAndDownload/static/js/2.c8c218b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0653f1502cac7c1f42",
+    "url": "/ImageSearchAndDownload/static/js/main.c8d313aa.chunk.js"
+  },
+  {
+    "revision": "fb91b787f675298aca2c",
+    "url": "/ImageSearchAndDownload/static/js/runtime-main.d0908aa8.js"
+  },
+  {
+    "revision": "a939e48cb12778cbec5c65a7b5e3283e",
+    "url": "/ImageSearchAndDownload/static/media/search.a939e48c.svg"
+  }
+]);
