d2a1faef4d7bfcd0cf2a79a41a57fe497338d8b6
stamic1 <bhatgaurav96@gmail.com>
2020-08-02 14:02:29 +0530

    bug fix

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -1,15 +1,30 @@
 import SearchBar from "./SearchBar";
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import axios from "axios";
 import ImageList from "./ImageList";
 
 function HomePage() {
   const [images, setImages] = useState([]);
+  const [initialLoad, setinitialLoad] = useState(true);
   const [query, setQuery] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const per_page = 9;
 
+  useEffect(async () => {
+    if (initialLoad) {
+      const response = await axios.get("https://api.unsplash.com/photos", {
+        params: { page: currentPage, per_page: per_page },
+        headers: {
+          Authorization:
+            "Client-ID o7dYoGTAAYKjip1p_dFQ6BJvbbAvLuxY6v_SDnbxsCw",
+        },
+      });
+
+      setImages(response.data);
+    }
+  }, []);
   const onSearchSubmit = async (term) => {
+    setinitialLoad(false);
     const response = await axios.get(
       "https://api.unsplash.com/search/photos/",
       {
@@ -26,18 +41,31 @@ function HomePage() {
 
   const onPageChange = async (term) => {
     let page = currentPage + 1;
-    const response = await axios.get(
-      "https://api.unsplash.com/search/photos/",
-      {
-        params: { page: page, per_page: per_page, query: query },
+    if (initialLoad == false) {
+      const response = await axios.get(
+        "https://api.unsplash.com/search/photos/",
+        {
+          params: { page: page, per_page: per_page, query: query },
+          headers: {
+            Authorization:
+              "Client-ID o7dYoGTAAYKjip1p_dFQ6BJvbbAvLuxY6v_SDnbxsCw",
+          },
+        }
+      );
+      setCurrentPage(page);
+      setImages(response.data.results);
+    } else {
+      const response = await axios.get("https://api.unsplash.com/photos", {
+        params: { page: page, per_page: per_page },
         headers: {
           Authorization:
             "Client-ID o7dYoGTAAYKjip1p_dFQ6BJvbbAvLuxY6v_SDnbxsCw",
         },
-      }
-    );
-    setCurrentPage(page);
-    setImages(response.data.results);
+      });
+      setCurrentPage(page);
+      setImages(response.data);
+    }
+  
   };
 
   return (
